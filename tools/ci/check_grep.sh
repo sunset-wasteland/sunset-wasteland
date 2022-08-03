3bbe7b29d472252382fe8188e0daa77945eac363
out-of-phaze <out.of.p.haze@proton.me>
2022-08-02 22:08:11 -0500

    Change git grep to grep for various checks

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -10,19 +10,19 @@ if git grep -Pn "\r\n"; then
 	echo "ERROR: CRLF line endings detected. Please stop using the webeditor, and fix it using a desktop Git client."
 	st = 1
 fi;
-if git grep -El '^\".+\" = \(.+\)' _maps/**/*.dmm;	then
+if grep -El '^\".+\" = \(.+\)' _maps/**/*.dmm;	then
     echo "ERROR: Non-TGM formatted map detected. Please convert it using Map Merger!"
     st=1
 fi;
-if git grep -Pn '^\ttag = \"icon' _maps/**/*.dmm;	then
+if grep -Pn '^\ttag = \"icon' _maps/**/*.dmm;	then
     echo "ERROR: tag vars from icon state generation detected in maps, please remove them."
     st=1
 fi;
-if git grep -Pn 'step_[xy]' _maps/**/*.dmm;	then
+if grep -Pn 'step_[xy]' _maps/**/*.dmm;	then
     echo "ERROR: step_x/step_y variables detected in maps, please remove them."
     st=1
 fi;
-if git grep -Pn 'pixel_[^xy]' _maps/**/*.dmm;	then
+if grep -Pn 'pixel_[^xy]' _maps/**/*.dmm;	then
     echo "ERROR: incorrect pixel offset variables detected in maps, please remove them."
     st=1
 fi;
@@ -32,22 +32,22 @@ fi;
 #    echo "ERROR: vareditted cables detected, please remove them."
 #    st=1
 #fi;
-if git grep -Pn '\td[1-2] =' _maps/**/*.dmm;	then
+if grep -Pn '\td[1-2] =' _maps/**/*.dmm;	then
     echo "ERROR: d1/d2 cable variables detected in maps, please remove them."
     st=1
 fi;
 echo "Checking for stacked cables"
-if git grep -Pn '"\w+" = \(\n([^)]+\n)*/obj/structure/cable,\n([^)]+\n)*/obj/structure/cable,\n([^)]+\n)*/area/.+\)' _maps/**/*.dmm;	then
+if grep -Pn '"\w+" = \(\n([^)]+\n)*/obj/structure/cable,\n([^)]+\n)*/obj/structure/cable,\n([^)]+\n)*/area/.+\)' _maps/**/*.dmm;	then
     echo "found multiple cables on the same tile, please remove them."
     st=1
 fi;
 echo "Checking for varedited /area paths"
-if git grep -Pn '^/area/.+[\{]' _maps/**/*.dmm;	then
+if grep -Pn '^/area/.+[\{]' _maps/**/*.dmm;	then
     echo "ERROR: Vareditted /area path use detected in maps, please replace with proper paths."
     st=1
 fi;
 echo "Checking for base /turf path in maps"
-if git grep -Pn '\W\/turf\s*[,\){]' _maps/**/*.dmm; then
+if grep -Pn '\W\/turf\s*[,\){]' _maps/**/*.dmm; then
     echo "ERROR: base /turf path use detected in maps, please replace with proper paths."
     st=1
 fi;
@@ -81,7 +81,7 @@ if git grep -in 'vault[\s-]?tek' *.dm; then
     st=1
 fi;
 echo "Checking for Vault-Tec typos in maps"
-if git grep -in 'vault[\s-]?tek' _maps/**/*.dmm; then
+if grep -in 'vault[\s-]?tek' _maps/**/*.dmm; then
     echo "ERROR: Misspelling(s) of Vault-Tec detected in maps, please replace the K with a C."
     st=1
 fi;

HolyTigerGod/Voidcrew

Componentizes Duffelbag Curse and turns it into an RPGloot modifier (+ curse announcement element) (#59029)
Co-authored-by: Mothblocks <35135081+Mothblocks@users.noreply.github.com>

code/modules/antagonists/fugitive/fugitive_outfits.dm
@@ -26,29 +26,28 @@
 	ears = /obj/item/radio/headset
 	glasses = /obj/item/clothing/glasses/regular/circle
 
-/datum/outfit/waldo/post_equip(mob/living/carbon/human/H, visualsOnly=FALSE)
+/datum/outfit/waldo/post_equip(mob/living/carbon/human/equipped_on, visualsOnly=FALSE)
 	if(visualsOnly)
 		return
-	H.fully_replace_character_name(null,"Waldo")
-	H.eye_color = "000"
-	H.gender = MALE
-	H.skin_tone = "caucasian3"
-	H.hairstyle = "Business Hair 3"
-	H.facial_hairstyle = "Shaved"
-	H.hair_color = "000"
-	H.facial_hair_color = H.hair_color
-	H.update_body()
-	if(H.mind)
-		H.mind.AddSpell(new /obj/effect/proc_holder/spell/aoe_turf/knock(null))
+	equipped_on.fully_replace_character_name(null,"Waldo")
+	equipped_on.eye_color = "000"
+	equipped_on.gender = MALE
+	equipped_on.skin_tone = "caucasian3"
+	equipped_on.hairstyle = "Business Hair 3"
+	equipped_on.facial_hairstyle = "Shaved"
+	equipped_on.hair_color = "000"
+	equipped_on.facial_hair_color = equipped_on.hair_color
+	equipped_on.update_body()
+	if(equipped_on.mind)
+		equipped_on.mind.AddSpell(new /obj/effect/proc_holder/spell/aoe_turf/knock(null))
 	var/list/no_drops = list()
-	no_drops += H.get_item_by_slot(ITEM_SLOT_FEET)
-	no_drops += H.get_item_by_slot(ITEM_SLOT_ICLOTHING)
-	no_drops += H.get_item_by_slot(ITEM_SLOT_OCLOTHING)
-	no_drops += H.get_item_by_slot(ITEM_SLOT_HEAD)
-	no_drops += H.get_item_by_slot(ITEM_SLOT_EYES)
-	for(var/i in no_drops)
-		var/obj/item/I = i
-		ADD_TRAIT(I, TRAIT_NODROP, CURSED_ITEM_TRAIT)
+	no_drops += equipped_on.get_item_by_slot(ITEM_SLOT_FEET)
+	no_drops += equipped_on.get_item_by_slot(ITEM_SLOT_ICLOTHING)
+	no_drops += equipped_on.get_item_by_slot(ITEM_SLOT_OCLOTHING)
+	no_drops += equipped_on.get_item_by_slot(ITEM_SLOT_HEAD)
+	no_drops += equipped_on.get_item_by_slot(ITEM_SLOT_EYES)
+	for(var/obj/item/trait_needed as anything in no_drops)
+		ADD_TRAIT(trait_needed, TRAIT_NODROP, CURSED_ITEM_TRAIT(trait_needed.type))
 
 /datum/outfit/synthetic
 	name = "Factory Error Synth"

sunset-wasteland/sunset-wasteland

code/modules/antagonists/fugitive/fugitive_outfits.dm
@@ -26,31 +26,30 @@
 	ears = /obj/item/radio/headset
 	glasses = /obj/item/clothing/glasses/regular/circle
 
-/datum/outfit/waldo/post_equip(mob/living/carbon/human/H, visualsOnly=FALSE)
+/datum/outfit/waldo/post_equip(mob/living/carbon/human/equipped_on, visualsOnly=FALSE)
 	if(visualsOnly)
 		return
-	H.fully_replace_character_name(null,"Waldo")
-	H.left_eye_color = "000"
-	H.right_eye_color = "000"
-	H.gender = MALE
-	H.skin_tone = "caucasian3"
-	H.hair_style = "Business Hair 3"
-	H.facial_hair_style = "Shaved"
-	H.hair_color = "000"
-	H.facial_hair_color = H.hair_color
-	H.update_body()
-	if(H.mind)
-		H.mind.AddSpell(new /obj/effect/proc_holder/spell/aoe_turf/knock(null))
+	equipped_on.fully_replace_character_name(null,"Waldo")
+	equipped_on.left_eye_color = "000"
+	equipped_on.right_eye_color = "000"
+	equipped_on.gender = MALE
+	equipped_on.skin_tone = "caucasian3"
+	equipped_on.hair_style = "Business Hair 3"
+	equipped_on.facial_hair_style = "Shaved"
+	equipped_on.hair_color = "000"
+	equipped_on.facial_hair_color = equipped_on.hair_color
+	equipped_on.update_body()
+	if(equipped_on.mind)
+		equipped_on.mind.AddSpell(new /obj/effect/proc_holder/spell/aoe_turf/knock(null))
 	var/list/no_drops = list()
-	no_drops += H.get_item_by_slot(ITEM_SLOT_FEET)
-	no_drops += H.get_item_by_slot(ITEM_SLOT_ICLOTHING)
-	no_drops += H.get_item_by_slot(ITEM_SLOT_OCLOTHING)
-	no_drops += H.get_item_by_slot(ITEM_SLOT_HEAD)
-	no_drops += H.get_item_by_slot(ITEM_SLOT_EYES)
-	for(var/i in no_drops)
-		var/obj/item/I = i
-		if(I)
-			ADD_TRAIT(I, TRAIT_NODROP, CURSED_ITEM_TRAIT)
+	no_drops += equipped_on.get_item_by_slot(ITEM_SLOT_FEET)
+	no_drops += equipped_on.get_item_by_slot(ITEM_SLOT_ICLOTHING)
+	no_drops += equipped_on.get_item_by_slot(ITEM_SLOT_OCLOTHING)
+	no_drops += equipped_on.get_item_by_slot(ITEM_SLOT_HEAD)
+	no_drops += equipped_on.get_item_by_slot(ITEM_SLOT_EYES)
+	for(var/obj/item/trait_needed as anything in no_drops)
+		if(trait_needed)
+			ADD_TRAIT(trait_needed, TRAIT_NODROP, CURSED_ITEM_TRAIT(trait_needed.type))
 
 /datum/outfit/synthetic
 	name = "Factory Error Synth"
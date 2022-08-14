8d03cca538eef09570a86b51e69062add4a5fc1e
out-of-phaze <out.of.p.haze@proton.me>
2022-08-13 21:40:20 -0500

    Bump rust-g and SpacemanDMM versions

--- dependencies.sh
+++ dependencies.sh
@@ -8,14 +8,14 @@ export BYOND_MAJOR=514
 export BYOND_MINOR=1571
 
 #rust_g git tag
-export RUST_G_VERSION=0.4.10
+export RUST_G_VERSION=0.5.0
 
 #node version
 export NODE_VERSION=14
 export NODE_VERSION_PRECISE=14.16.1
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.7
+export SPACEMAN_DMM_VERSION=suite-1.7.2
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.7.9
